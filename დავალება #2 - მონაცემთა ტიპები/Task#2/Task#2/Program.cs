7860e2acea78c321cb0fae863edfaefa53101f41
Natia Akhalaia <nakhalaia@credo.ge>
2024-11-05 12:22:19 +0400

    added.Task#2

---
+++ დავალება #2 - მონაცემთა ტიპები/Task#2/Task#2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+
+    string Name = "Natia";
+    byte Age = 37;
+    int Height = 170;
+    int Weight = 55;
+
+    Console.WriteLine("Name:" + Name);
+    Console.WriteLine("Age:" + Age);
+    Console.WriteLine("Height:" + Height);
+    Console.WriteLine("Weight:" + Weight);
+
+}
+
