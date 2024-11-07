c8202f20b5ba76ffe40d8e937a72637354e457d8
Natia Akhalaia <nakhalaia@credo.ge>
2024-11-07 23:37:23 +0400

    case #8

---
+++ დავალება #8 -stack and heap/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace PersonApp
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            string name = "Sesili";
+            int age = 13;
+
+            Person person1 = new Person
+            {
+                Name = name,
+                Age = age
+            };
+
+            Console.WriteLine("pirveli");
+            Console.WriteLine("Name: " + person1.Name);
+            Console.WriteLine("Age: " + person1.Age);
+
+            Person person2 = person1;
+            person2.Name = "Anna";
+            person2.Age = 8;
+
+          
+            Console.WriteLine("meore");
+            Console.WriteLine("Name: " + person2.Name);
+            Console.WriteLine("Age: " + person2.Age);
+
+
+        }
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
