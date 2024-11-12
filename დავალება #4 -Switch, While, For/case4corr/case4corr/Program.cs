4622aab20714e34d96c8d49ad6a34dce54670711
Natia Akhalaia <nakhalaia@credo.ge>
2024-11-12 21:46:01 +0400

    corrcase4

---
+++ დავალება #4 -Switch, While, For/case4corr/case4corr/Program.cs
@@ -0,0 +1,23 @@
+﻿{
+    static void Main()
+    {
+        int sum = 0;
+        int number;
+
+
+        while (true)
+        {
+            Console.Write("sheikvane dadebiti ricxvi: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                break;
+            }
+
+            sum += number;
+        }
+
+        Console.WriteLine($"sul: {sum}");
+    }
+}
