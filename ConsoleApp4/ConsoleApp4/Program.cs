1e3b34952b9e8cc0b9601d192764de87174e5bad
Natia Akhalaia <nakhalaia@credo.ge>
2024-11-14 15:17:55 +0400

    case#9

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+interface IAnimal
+{
+    string MakeSound();
+}
+
+abstract class Animal : IAnimal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+   
+    public abstract string MakeSound();
+}
+
+class Dog : Animal
+{
+    public Dog(string name, int age) : base(name, age) { }
+
+   
+    public override string MakeSound()
+    {
+        return "Woof";
+    }
+}
+
+class Cat : Animal
+{
+    public Cat(string name, int age) : base(name, age) { }
+
+    
+    public override string MakeSound()
+    {
+        return "Meow";
+    }
+}
+
+class Cow : Animal
+{
+    public Cow(string name, int age) : base(name, age) { }
+
+    
+    public override string MakeSound()
+    {
+        return "Moo";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        IAnimal[] animals = new IAnimal[]
+        {
+            new Dog("Jina", 3),
+            new Cat("Chiko", 2),
+            new Cow("Wikara", 5)
+        };
+
+
+        foreach (IAnimal animal in animals)
+        {
+            Console.WriteLine($"{animal.GetType().Name} says: {animal.MakeSound()}");
+        }
+    }
+}
+
