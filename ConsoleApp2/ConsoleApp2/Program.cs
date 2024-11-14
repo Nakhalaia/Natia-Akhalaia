362ef3b04a4d5a8430b6bef5c561ada53cd51719
Natia Akhalaia <nakhalaia@credo.ge>
2024-11-14 13:57:48 +0400

    case#5

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+
+//        string[] students = new string[3];
+
+
+//        for (int i = 0; i < 3; i++)
+//        {
+//            Console.Write($" {i + 1} studentis saxeli: ");
+//            students[i] = Console.ReadLine(); 
+//        }
+
+
+//       Console.WriteLine("\nstudentebis saxelebi:");
+//        foreach (string student in students)
+//        {
+//            Console.WriteLine(student);
+//        }
+//    }
+//}
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[,] grades = new int[3, 2];
+
+        
+        string[] students = new string[3] { "Student 1", "Student 2", "Student 3" };
+
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"{students[i]}:");
+            Console.Write("First subject: ");
+            grades[i, 0] = int.Parse(Console.ReadLine());  
+
+            Console.Write("Second Subject: ");
+            grades[i, 1] = int.Parse(Console.ReadLine());  
+
+            Console.WriteLine(); 
+        }
+
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"{students[i]}: {grades[i, 0]} Firs subject Score, {grades[i, 1]} Second Subject Score.");
+        }
+    }
+}
+
+
+
+
